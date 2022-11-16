288835e7891dcf1506c0e0aaa5f08cb3c10676fe
Ahnaf Kazi <33816349+AhnafK@users.noreply.github.com>
2022-11-15 23:24:14 -0500

    Update LoginForm.js

--- techvology-app/src/pages/LoginForm.js
+++ techvology-app/src/pages/LoginForm.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import axios from 'axios';
 
 const LoginForm = (props) => {
 
@@ -6,55 +7,57 @@ const LoginForm = (props) => {
     const [errorMessages, setErrorMessages] = useState({});
     const [loggedIn, setLoggedIn] = useState(false);
     
-    // Placeholder accounts database
-   const database = [
-        {
-        username: "user",
-        password: "pass"
-
-        },
-        {
-        username: "admin",
-        password: "admin"
-        }
-    ];
- 
-    /*
-    var database = [
-        {
-            username: "user",
-            password: "pass",
-            actions: []
-
-        },
-        {
-            username: "admin",
-            password: "admin",
-            actions: []
-        }
-    ];
-    */
-
-    const error = "Your username and/or password are incorrect. Please try again.";
+    const incorrect = "Your username and/or password are incorrect. Please try again.";
     
     const handleSubmit = (event) => {
         //Prevent page reload
         event.preventDefault();
+        
+        // get data from loginForm
+        var username = document.getElementById("uname").value;
+        var password = document.getElementById("pass").value;
+        
+        const login = {'username': username, 'password': password};
+
+        // login with axios
+        axios.post('/login', { body: {username: ""+login['username'], password: ""+login['password']} }, {'content-type': 'application/json'})
+        .then(function(response) {
+            setLoggedIn(true);
+            setErrorMessages({name: "error", message: 'success'});
+          }).catch(function(error) {
+            setErrorMessages({ name: "error", message: incorrect });
+          });
+    };
+
+    const handleRegister = (event) => {
+        //Prevent page reload
+        event.preventDefault();
     
-        var { uname, pass } = document.forms[0];
-    
-        // Find user login info
-        const userData = database.find((user) => user.username === uname.value);
-    
-        // Compare user info
-        if (userData) {
-            if (userData.password === pass.value) {
-                setLoggedIn(true);
-            }
-        } else {
-        // Login information does not match
-        setErrorMessages({ name: "error", message: error });
+        var setname = document.getElementById("setname").value;
+        var setpass = document.getElementById("setpass").value;
+        var confirmpass = document.getElementById("confirmpass").value;
+        
+        if(setpass.value !== confirmpass.value){
+            setErrorMessages({ name: "reg error", message: "Passwords do not match."+setname.value+ setpass.value+ confirmpass.value });
+            return;            
         }
+        else{
+            const register = {
+                'username': setname,
+                'password': setpass
+              };
+    
+            // register with axios
+            setErrorMessages({ name: "error", message: "Registering" });
+            axios.post('/login/register', { body: {username: ""+register['username'], password: ""+register['password']} }, {'content-type': 'application/json'})
+            .then(function(response) {
+                setErrorMessages({ name: "reg error", message: "Registered" });
+                setLoggedIn(true);
+              }).catch(function(error) {
+                setErrorMessages({ name: "reg error", message: ""+error });
+              });
+        }
+        return;
     };
     
     // Generate JSX code for error message
@@ -66,36 +69,37 @@ const LoginForm = (props) => {
     // Login Form 
     const renderForm = (
         <div className="form">
-        <form onSubmit={handleSubmit}>
-            
-            <div className="input-container">
-            <label>Username </label>
-            <input 
-                type="text" 
-                name="uname" 
-                placeholder="Enter Username"
-                required 
-            />
-            </div>
+            <form onSubmit={handleSubmit} id="loginForm">
+                <h4>Log in</h4>
+                <div className="form-group">
+                    <label for="uname">Username</label>
+                    <input type="text" className="form-control" id="uname" placeholder="Enter username" />
+                </div>
+                <div className="form-group">
+                    <label for="pass">Password</label>
+                    <input type="password" className="form-control" id="pass" placeholder="Enter Password" />
+                </div>
+                <button type="submit" className="btn btn-primary">Submit</button>
+                {renderErrorMessage("error")}
 
-            <div className="input-container">
-            <label>Password </label>
-            <input 
-                type="password" 
-                name="pass" 
-                placeholder="Enter Password"
-                required 
-            />
-            </div>
-
-            <div className="button-container">
-            <input 
-                type="submit" 
-            />
-            {renderErrorMessage("error")}
-            </div>
-
-        </form>
+            </form>
+            <form onSubmit={handleRegister} id="registerForm">
+                <h4>Register</h4>
+                <div className="form-group">
+                    <label for="uname">Username</label>
+                    <input type="text" className="form-control" id="setname" placeholder="Set username" />
+                </div>
+                <div className="form-group">
+                    <label for="pass">Password</label>
+                    <input type="password" className="form-control" id="setpass" placeholder="Set Password" />
+                </div>
+                <div className="form-group">
+                    <label for="pass">Confirm Password</label>
+                    <input type="password" className="form-control" id="confirmpass" placeholder="Reenter Password" />
+                </div>
+                <button type="submit" className="btn btn-secondary">Submit</button>
+                {renderErrorMessage("reg error")}
+            </form>
         </div>
     );
     
