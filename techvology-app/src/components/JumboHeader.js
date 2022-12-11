5153ba1db63864017eabc1cd9161b34c50de8c6b
Ahnaf Kazi <33816349+AhnafK@users.noreply.github.com>
2022-12-11 15:30:34 -0500

    Updates Homepage

--- techvology-app/src/components/JumboHeader.js
+++ techvology-app/src/components/JumboHeader.js
@@ -1,24 +1,31 @@
 import React from 'react';
-import styled from 'styled-components';
-import natureImage from '../images/nature.jpg';
+import natureImage from '../images/Nature.jpeg';
 import DailyTip from './DailyTip';
+import "../assets/bootstrap/css/bootstrap.min.css";
 
 const JumboHeader = () => {
     return (
-        <div>
-            <div class="p-5 mb-4 bg-light rounded-3"
-                style={{
-                    backgroundImage: `url(${natureImage})`,
-                    backgroundSize: 'cover',
-                }}
-            >
-                <div class="container-fluid py-5">
-                    <h1 class="display-5 fw-bold">Techvology</h1>
-                    <p class="col-md-8 fs-4">Carbon Footprint: Bigger isn't better</p>
-                    <DailyTip/>
+        <div data-bss-parallax-bg="true" style= {{height: '500px',
+        backgroundImage: `url(${natureImage})`,
+        backgroundPosition: 'center', 
+        backgroundSize: 'cover'}}>
+        <section class="py-4 py-xl-5">
+            <div class="row" style={{paddingTop: '0px', marginTop: '53px'}}></div>
+            <div class="container h-100">
+                <div class="text-white bg-dark border rounded border-0 p-4 py-5" style={{paddingLeft: '82px', marginLeft: '276px', marginRight: '276px'}}>
+                    <div class="row h-100">
+                        <div class="col-md-10 col-xl-8 text-center d-flex d-sm-flex d-md-flex justify-content-center align-items-center mx-auto justify-content-md-start align-items-md-center justify-content-xl-center">
+                            <div>
+                                <h1 class="text-uppercase fw-bold text-white mb-3">Techvology</h1>
+                                <h4 class="mb-4">Carbon Footprint: Bigger isn't better</h4>
+                                <DailyTip />
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
+        </section>
+    </div>
     )
 }
 
--- techvology-app/src/pages/Home.js
+++ techvology-app/src/pages/Home.js
@@ -22,41 +22,32 @@ const Home = () => {
   return (
     <div>
       <JumboHeader />
-      <Container>
-        <Row>
-          <Col md={6}>
-            <p style={Text}>
-            Climate change refers to long-term shifts in temperatures and weather patterns. These shifts may be natural, such as through variations in the solar cycle. But since the 1800s, human activities have been the main driver of climate change, primarily due to burning fossil fuels like coal, oil and gas.
-            Burning fossil fuels generates greenhouse gas emissions that act like a blanket wrapped around the Earth, trapping the sun’s heat and raising temperatures.
-            Examples of greenhouse gas emissions that are causing climate change include carbon dioxide and methane. These come from using gasoline for driving a car or coal for heating a building, for example. Clearing land and forests can also release carbon dioxide. Landfills for garbage are a major source of methane emissions. Energy, industry, transport, buildings, agriculture and land use are among the main emitters.
-            </p>
-          </Col>
-          <Col md={6}>
-            <Image
-              src={climateStrike}
-              fluid
-            />
-          </Col>
-        </Row>
-        <Row>
-          <Col md={6}>
-            <p style={Text}>
-            Everyone can help limit climate change. From the way we travel, to the electricity we use, the food we eat, and the things we buy, we can make a difference.
+      <Container class="py-4 py-xl-5" style={{paddingTop: '40px'}}>
+        <div class="row gy-4 gy-md-0">
+            <div class="col-md-6 text-center text-md-start d-flex d-sm-flex d-md-flex justify-content-center align-items-center justify-content-md-start align-items-md-center justify-content-xl-center">
+                <div class="text-end">
+                    <p class="fs-4 my-3">Climate change refers to long-term shifts in temperatures and weather patterns. These shifts may be natural, such as through variations in the solar cycle. But since the 1800s, human activities have been the main driver of climate change, primarily due to burning fossil fuels like coal, oil and gas. Burning fossil fuels generates greenhouse gas emissions that act like a blanket wrapped around the Earth, trapping the sun’s heat and raising temperatures. Examples of greenhouse gas emissions that are causing climate change include carbon dioxide and methane. These come from using gasoline for driving a car or coal for heating a building, for example. Clearing land and forests can also release carbon dioxide. Landfills for garbage are a major source of methane emissions. Energy, industry, transport, buildings, agriculture and land use are among the main emitters.</p>
+                </div>
+            </div>
+            <div class="col-md-6"><img class="rounded img-fluid w-100 fit-cover" src={climateStrike}/></div>
+        </div>
+    </Container>
+    <div class="container py-4 py-xl-5">
+        <div class="row gy-4 gy-md-0">
+            <div class="col-md-6"><img class="rounded img-fluid w-100 fit-cover" src={climateChange}/></div>
+            <div class="col-md-6 d-md-flex align-items-md-center">
+                <div>
+                    <p class="fs-4 my-3">Everyone can help limit climate change. From the way we travel, to the electricity we use, the food we eat, and the things we buy, we can make a difference.
             Greenhouse gas emissions per person vary greatly among countries. In the United States of America, emissions in 2020 (the latest available data) were 14.6 tons of CO2 equivalent per person – more than double the global average of 6.3 tons, and six times the 2.4 tons per person in India.
             To preserve a livable climate, the average emissions per person per year will need to drop to around 2 to 2.5 tons of CO2e by 2030. Start with these ten actions to help tackle the climate crisis.
             </p>
-            <p style={Text}>
+            <p class="fs-4 my-3">
               - United Nations
             </p>
-          </Col>
-          <Col md={6}>
-            <Image
-              src={climateChange}
-              fluid
-            />
-          </Col>
-        </Row>
-      </Container>
+                </div>
+            </div>
+        </div>
+    </div>
     </div>
   )
 }
