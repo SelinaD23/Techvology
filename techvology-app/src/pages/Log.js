140bea469d6c11e05412865ed911f0518eb6204b
Ahnaf Kazi <33816349+AhnafK@users.noreply.github.com>
2022-12-11 20:04:53 -0500

    Update Log.js

--- techvology-app/src/pages/Log.js
+++ techvology-app/src/pages/Log.js
@@ -65,34 +65,46 @@ const Log = () => {
 
     return (
         <div>
-            <SearchBar actionsList={userActionsList} setSearchList={setSearchList} fetchActions={fetchUserActions} />
+            <section class="py-3"/>
             <div>
-                <input
-                    type="text"
-                    id="title"
-                    placeholder="Action Name"
-                    value={actionTitle}
-                    onChange={(e) => setActionTitle(e.target.value)}
-                />
-                <input
-                    type="text"
-                    id="carbon"
-                    placeholder="Carbon"
-                    value={actionCarbon}
-                    onChange={(e) => setActionCarbon(e.target.value)}
-                />
-                <button onClick={handleCustomAction}>Add Action</button>
+                <div class="card-body text-center bg-dark border rounded border-dark d-flex flex-column align-items-center" style={{margin: '20px', paddingTop: '30px', paddingBottom: '20px'}}>
+                <h2 class="fw-bold"><br /><strong>Add Custom Action</strong><br /><br /></h2>
+                    <form>
+                        <div class="mb-3">
+                            <div class="container">
+                                <div class="row">
+                                    <div class="col-md-6"><input class="form-control-sm" type="text" id="title" placeholder="Action Name" value={actionTitle} onChange={(e) => setActionTitle(e.target.value)}/></div>
+                                    <div class="col-md-6"><input class="form-control-sm" type="number" id="carbon" placeholder="Carbon" value={actionCarbon} onChange={(e) => setActionCarbon(e.target.value)} /></div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="mb-3"></div>
+                        <div class="mb-3"><button class="btn btn-primary shadow d-block w-100" onClick={handleCustomAction}>Add Action</button></div>
+                    </form>
+                </div>
             </div>
+            <SearchBar actionsList={userActionsList} setSearchList={setSearchList} fetchActions={fetchUserActions} />
             <List>
                 {searchList.slice(0).reverse().map((action) => (
-                    <ListItem key={searchList.indexOf(action)}>
-                        <ListItemText
-                            primary={action.title + ": " + action.carbon_output + " lbs of carbon"}
-                        />
-                        <IconButton edge="end" aria-label="delete" onClick={() => handleDelete(searchList.indexOf(action))}>
-                            <DeleteIcon />
-                        </IconButton>
-                    </ListItem>
+                        <div class="card" style={{ marginLeft: '32px', marginRight: '32px',paddingBottom: '15px' }}>
+                            <div class="card-body bg-dark border rounded border-dark shadow-none">
+                                <div class="container">
+                                    <div class="row" style={{ marginLeft: '0px', marginRight: '0px' , marginTop: '10px', marginBottom: '10px'}}>
+                                        <div class="col-md-6 text-start" style={{ paddingLeft: '0px', paddingRight: '0px' }}>
+                                            <h4>{action.title}</h4>
+                                            <h6 class="text-muted mb-2">{action.carbon_output + " lbs of carbon"}</h6>
+                                        </div>
+                                        <div class="col-md-6 text-end d-xl-flex justify-content-xl-end align-items-xl-center">
+                                            <IconButton edge="end" aria-label="delete" onClick={() => handleDelete(searchList.indexOf(action))}>
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16" class="bi bi-trash-fill" data-bs-toggle="tooltip" data-bss-tooltip="" style={{ fontSize: '25px', marginTop: '8px', marginBottom: '8px', marginLeft: '8px', marginRight: '8px' }} title="Remove">
+                                                    <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"></path>
+                                                </svg>
+                                            </IconButton>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                 ))}
             </List>
         </div>
